ef35afe6e1605902623380dfd368faa46a81e75c
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-05-27 00:31:21 +0100

    add goods to files

--- main.c
+++ main.c
@@ -3,102 +3,110 @@
 #include <string.h>
 
 
-
-int main()
+typedef struct
 {
-typedef struct goods
+    char  *code; //pointeur
+    char  *libelle;
+    float prix;
+}typeDuBien;
+
+static typeDuBien biens[] =
 {
-char goods_code[6];
-char goods_descrip[20];
-float price;
-}goodtype;
+    { "00310", "Poulette",  35.00 },
+    { "00311", "porc",  20.50 },
+    { "00322", "Du boeuf",  45.00 },
+    { "00323", "Poisson",  40.00 },
+    { "00510", "T-shirt",  75.00 },
+    { "00511", "Pantalon", 120.00 },
+    { "00512", "Manteau", 100.00 },
+    { "00513", "Sauteuse",  85.00 },
+    { "00710", "Agresser",  15.50 },
+    { "00711", "Poêle à frire", 200.00 },
+    { "00712", "bol",  25.00 },    
+    { "1009", "creme_glacee",  3.56 },
+    { "167", "fromage",  1.55 },
+    { "123", "jus",  1.35 },
+    { "500", "lait",  0.97 },
+    { "890", "coca",  1.22 },
+    { "654", "chocolat",  2.78 },
+    { "763", "crevette",  33.62 },
+    { "34", "serviette_papier",  0.87 },
+};
+static int bienstotal = sizeof(biens) / sizeof(biens[0]);
+int main( void )
 
-goodtype goods[13];
-strcpy(goods[0].goods_descrip, "Chicken");
-strcpy(goods[0].goods_code, "00310");
-goods[0].price = 35.00;
+{
+	//afficher table
+	unsigned int i,j,minSupp;
+    for (i = 0; i < bienstotal; i++ )
+        printf( "%s %s %f\n", biens[i].code, biens[i].libelle, biens[i].prix );
+    	printf( "\n Nombres totales des biens: %d\n", bienstotal );
 
-strcpy(goods[1].goods_descrip, "Pork");
-strcpy(goods[1].goods_code, "00311");
-goods[1].price = 20.50;
 
-strcpy(goods[2].goods_descrip, "Beef");
-strcpy(goods[2].goods_code, "00322");
-goods[2].price = 45.00;
 
-strcpy(goods[3].goods_descrip, "Fish");
-strcpy(goods[3].goods_code, "00323");
-goods[3].price = 40.00;
+char tempCode[6]; //code entrer
 
-strcpy(goods[4].goods_descrip, "Walmart T Shirt");
-strcpy(goods[4].goods_code, "00510");
-goods[4].price = 75.00;
-
-strcpy(goods[5].goods_descrip, "Walmart Trousers");
-strcpy(goods[5].goods_code, "00511");
-goods[5].price = 120.00;
-
-strcpy(goods[6].goods_descrip, "Walmart Coat");
-strcpy(goods[6].goods_code, "00512");
-goods[6].price = 100.00;
-
-strcpy(goods[7].goods_descrip, "Walmart Jumper");
-strcpy(goods[7].goods_code, "00513");
-goods[7].price = 85.00;
-
-strcpy(goods[8].goods_descrip, "Mug");
-strcpy(goods[8].goods_code, "00710");
-goods[8].price = 15.50;
-
-strcpy(goods[9].goods_descrip, "Fry Pan");
-strcpy(goods[9].goods_code, "00711");
-goods[9].price = 200.00;
-
-strcpy(goods[10].goods_descrip, "Bowl");
-strcpy(goods[10].goods_code, "00712");
-goods[10].price = 25.00;
-
-strcpy(goods[11].goods_descrip, "Dish");
-strcpy(goods[11].goods_code, "00713");
-goods[11].price = 25.00;
-
-char tempCode[6];
 typedef struct receipt
 {
-char receiptNM[20], receiptCD[6];
-float receiptPC;
+char receiptLibelle[20], receiptCode[6];
+float receiptPrix;
 }receipttype;
+
 receipttype receipt[13];
-char stop[2] = {"F"};
-float ttlcost = 0;
-int i,j;
-unsigned int count = 0;
+char stop[2] = {"F"}; //sorter du programme
+float ttlcost = 0; //resultat final
+
+unsigned int total = 0; //nbr de produit dans le tableaux
+    FILE * bdt;	//file bdt
+	FILE * idt;	//file identi
+	FILE * rslt; //file resultat		
 
 
-
-while ( strcmp(tempCode, stop) ){
-printf("Please enter the item code. Type F to finish: ");
+while ( strcmp(tempCode, stop) ){ //strcmp comparer entre 2 string
+printf("\n Veuillez saisir le code article. Tapez F pour terminer: ");
 scanf("%s", tempCode);
-for (i = 0; i < 12; ++i){
-    if (strcmp(tempCode, goods[i].goods_code) == 0){
-        strcpy(receipt[count].receiptNM, goods[i].goods_descrip);
-        strcpy(receipt[count].receiptCD, goods[i].goods_code);
-        receipt[count].receiptPC = goods[i].price;
-        ttlcost += goods[i].price;
-        count++;
+printf("Donner nombre de produits: ");
+scanf("%f", &minSupp);
+
+for (i = 0; i < bienstotal; ++i){
+	if(strcmp(tempCode, biens[i].code) == 0){
+        strcpy(receipt[total].receiptLibelle, biens[i].libelle); //strcpy copy
+        strcpy(receipt[total].receiptCode, biens[i].code);
+        receipt[total].receiptPrix = biens[i].prix ;
+        ttlcost += biens[i].prix;
+        total++;
     }
 
 }
 
 }
 printf("_____________________________________\n\n");
-printf("      THANK YOU FOR VISITING US!     \n");
+printf("      MERCI DE VOTRE VISITE!     \n");
 printf("_____________________________________\n");
-printf("         Here is your receipt:       \n\n");
-printf("%10s%20s%10s", "Item", "Code", "Price\n");
-for (j= 0; j < count; ++j){
-printf("%10s%20s%10.2f\n", receipt[j].receiptNM, receipt[j].receiptCD, receipt[j].receiptPC);
+printf("         Voici votre reçu:       \n\n");
+printf("%10s%20s%10s", "Libelle", "Code","Prix\n");
+
+//files 
+   bdt = fopen ("Bdt.txt","a");  /* Bdt.txt*/
+   idt = fopen ("identifiant.txt","w");  /* identifiant.txt*/
+   rslt = fopen("resultat.txt","a");
+
+for (j= 0; j < total; ++j){
+printf("%10s%20s%10.2f\n", receipt[j].receiptLibelle, receipt[j].receiptCode, receipt[j].receiptPrix);
+
+
+
+fprintf(idt,"%20s%10s%10.2f\n", receipt[j].receiptCode, receipt[j].receiptLibelle,  receipt[j].receiptPrix); //for identifiant
+fprintf (bdt, "%20s",  receipt[j].receiptCode); //for Bdt
+fprintf(rslt,"%20s%10s%10.2f\n", receipt[j].receiptCode, receipt[j].receiptLibelle,  receipt[j].receiptPrix); //for resultat
 }
 printf("\n_____________________________________\n");
-printf("          TOTAL COST:%.2f            \n", ttlcost);
+printf("          TOTALE:%.2f            \n", ttlcost);
+fprintf(rslt,"TOTALE:%.2f\n", ttlcost); //for resultat
+/* close  files*/  
+   fclose (idt);
+   fclose(rslt);
+   fclose(bdt);
+   fprintf (bdt, "\n");
+   return 0;
 }
